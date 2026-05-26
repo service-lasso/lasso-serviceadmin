750b2dd38379bd93f969182b0a6d795790d99359
Max Barrass <max.barrass@gmail.com>
2026-05-26 18:58:44 +1000

    ISS-101 rename Operations nav section (#110)
    * ISS-101 rename operations nav section
    
    * Merge master into Service Admin nav label PR

--- src/components/layout/data/sidebar-data.ts
+++ src/components/layout/data/sidebar-data.ts
@@ -38,7 +38,7 @@ export const sidebarData: SidebarData = {
   ],
   navGroups: [
     {
-      title: 'Operations',
+      title: 'Service Admin',
       items: [
         {
           title: 'Dashboard',
